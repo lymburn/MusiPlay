e890151900ec5ef715589449087e2377af2296ea
Eugene Lu <eugenelu9848@gmail.com>
2018-05-16 12:47:57 -0600

    Initial Commit

---
+++ Music Player/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Music Player
+//
+//  Created by Eugene Lu on 2018-05-16.
+//  Copyright © 2018 Eugene Lu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
